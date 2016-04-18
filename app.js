9cacd48c05add11e80f76003f73317cc959bf0e8
alexundros <alex-vlz@mail.ru>
2016-04-18 15:34:01 +0300

    -

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ var express = require('express');
 var app = express();
 
 app.set('port', process.env.PORT || 3333);
+app.use(express.static(__dirname + '/www'));
+
 app.set('views', __dirname + '/template');
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs-locals'));
-
-process.env.PWD = process.cwd();
-app.use(express.static(process.env.PWD + '/www'));
-
 
 app.get('/', function (req, res) {
 	res.render('index', {title: 'Test'});
